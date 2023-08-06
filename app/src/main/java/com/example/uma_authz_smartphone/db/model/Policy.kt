36ea6b8822d6c8a93dbb17f86aa678a996d1f9ef
laft <44256206+laft2@users.noreply.github.com>
2023-08-06 15:25:15 +0900

    realm

---
+++ app/src/main/java/com/example/uma_authz_smartphone/db/model/Policy.kt
@@ -0,0 +1,9 @@
+package com.example.uma_authz_smartphone.db.model
+
+import io.realm.kotlin.types.RealmObject
+import io.realm.kotlin.types.annotations.PersistedName
+
+@PersistedName(name = "Policy")
+class DbPolicy(): RealmObject{
+    
+}
